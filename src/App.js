24f2fd194fdc0802041348505fd4d8d606a4e0fb
Bhesh Lama <bheshlama195@gmail.com>
2023-10-09 10:45:30 +0545

    s

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
     <>
       <BrowserRouter>
         <Suspense fallback="Loading...">
-          <Popup />
+          {/* <Popup /> */}
 
           <Navbar />
           <ScrollToTop />
